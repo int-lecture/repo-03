096257fbfe913baba69c1ea944c77b7b11f6ad25
Luca-Laptop <1610927@stud.hs-mannheim.de>
2017-04-07 12:13:47 +0200

    Rechtschreibfeher korrigiert und Daten anonymisiert

--- website/newsletter.php
+++ website/newsletter.php
@@ -1,16 +1,16 @@
 	<?php
-		<! Variablen auslesen und setzen>
+		
 		$strVorname = $_POST["vorname"];
 		$strNachname = $_POST["nachname"];
 		$strEmail = $_POST["email"];
-		<! Überprüfen ob alle Felder ausgefüllt und Datenspeicherung akzeptiert wurde>
+		
 		if($strVorname == "" || $strNachname == "" || $strEmail == "" || !(isset( $_POST[datenspeicherung]))) {
 			header("Location: registrierenFail.html");
 			exit;
 			
 		}
 		else {
-			<! Schreibe die Daten der Person in die Textdatei "newsletterAbos.txt>
+			
 			$handle = fopen ('newsletterAbos.txt', 'a+'	);
 			fwrite($handle, $strVorname . ",");
 			fwrite($handle, $strNachname. ",");
