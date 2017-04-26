7ab19d89caa008d0a565c3c78120d1ebc9a3005e
Christian Coenen <coenen.christian@outlook.com>
2017-04-26 21:04:06 +0200

    bugfix

--- chat-server/testFull.sh
+++ chat-server/testFull.sh
@@ -3,4 +3,4 @@
 ./testSend.sh
 ./testSend.sh
 ./testSend.sh
-curl -X GET http://172.16.128.211:5000/messages/bob/2
+curl -X GET localhost:5000/messages/bob/2
--- chat-server/testSend.sh
+++ chat-server/testSend.sh
@@ -1,1 +1,1 @@
-curl -v -H "Content-Type: application/json" -X PUT -d '{"to":"bob","from":"tom","date":"2017-04-26T11:30:30Z","text":"Test1"}' http://172.16.128.211:5000/send
+curl -v -H "Content-Type: application/json" -X PUT -d '{"to":"bob","from":"tom","date":"2017-04-26T11:30:30Z","text":"Test1"}' localhost:5000/send
