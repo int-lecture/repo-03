1be4d09ab147bd0fea2a655becde3f85e27e49fc
Moritz <1611229@stud.hs-mannheim.de>
2017-05-15 19:33:00 +0200

    Added remote deploy

--- services/build.sh
+++ services/build.sh
@@ -6,4 +6,5 @@ echo "Binaries finished building."
 echo "Deploying."
 mv login-server/target/login-server*.jar login-server.jar
 mv chat-server/target/chat-server*.jar chat-server.jar
+scp *.jar docker-03:/usr/local/source/repo-03/services/target/
 echo "Deployed."
