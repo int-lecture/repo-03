d0442601dce03f634979dde3ec824228f69d92de
Moritz <1611229@stud.hs-mannheim.de>
2017-05-15 21:18:33 +0200

    Added systemd service files to deploy

--- services/build.sh
+++ services/build.sh
@@ -7,4 +7,5 @@ echo "Deploying."
 mv login-server/target/login-server*.jar login-server.jar
 mv chat-server/target/chat-server*.jar chat-server.jar
 scp *.jar docker-03:/usr/local/source/repo-03/services/
+scp *.service docker-03:/usr/local/source/repo-03/services/
 echo "Deployed."
