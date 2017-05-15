de01081e490590bbc620ee0de8fe5f97a12e8ada
Moritz <1611229@stud.hs-mannheim.de>
2017-05-15 21:11:00 +0200

    Fixed build path

--- services/build.sh
+++ services/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-(cd /chat-server/; mvn clean compile assembly:single)
-(cd /login-server/; mvn clean compile assembly:single)
+(cd chat-server/; mvn clean compile assembly:single)
+(cd login-server/; mvn clean compile assembly:single)
 echo "Binaries finished building."
 echo "Deploying."
 mv login-server/target/login-server*.jar login-server.jar
