34e5a0bd88a5a7386b5db73acd44500fecea73a7
Moritz <1611229@stud.hs-mannheim.de>
2017-05-27 15:16:06 +0200

    Fix build script

--- services/build.sh
+++ services/build.sh
@@ -7,7 +7,7 @@ echo "Binaries finished building."
 echo "Deploying."
 mv login-server/target/login-server*.jar login-server.jar
 mv chat-server/target/chat-server*.jar chat-server.jar
-mv chat-server/target/register-server*.jar chat-server.jar
+mv register-server/target/register-server*.jar register-server.jar
 scp *.jar docker-03:/usr/local/source/repo-03/services/
 scp *.service docker-03:/usr/local/source/repo-03/services/
 echo "Deployed."
