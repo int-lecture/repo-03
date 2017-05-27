5ec472a12e6f932a55d43f1333c4074224a38d35
Moritz <1611229@stud.hs-mannheim.de>
2017-05-27 15:11:29 +0200

    Made deployment scripts executable

--- services/build.sh
+++ services/build.sh
@@ -2,10 +2,12 @@
 git pull
 (cd chat-server/; mvn clean compile assembly:single)
 (cd login-server/; mvn clean compile assembly:single)
+(cd register-server/; mvn clean compile assembly:single)
 echo "Binaries finished building."
 echo "Deploying."
 mv login-server/target/login-server*.jar login-server.jar
 mv chat-server/target/chat-server*.jar chat-server.jar
+mv chat-server/target/register-server*.jar chat-server.jar
 scp *.jar docker-03:/usr/local/source/repo-03/services/
 scp *.service docker-03:/usr/local/source/repo-03/services/
 echo "Deployed."
