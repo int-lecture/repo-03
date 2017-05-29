0ebead5df034f03b98367bdcd0645f53d11ef981
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-05-29 17:58:30 +0200

    Add files via upload

--- website/chatFrontend/chatApplication/js/chatApplication.js
+++ website/chatFrontend/chatApplication/js/chatApplication.js
@@ -5,20 +5,9 @@ $( document ).ready(function() {
 var token;
 var pseudonym;
 var contacts;
+var sequenceNumbers;
 function loadContacts(){
-	var decodedCookie = decodeURIComponent(document.cookie);
-    	var ca = decodedCookie.split(';');
-	$.each(ca, function(index, value){
-		value = value.trim();
-		if(value.substring(0,6)=="token="){
-			token=value.substring(6);
-			alert(token);		
-		}
-		if(value.substring(0, "pseudonym=".length)=="pseudonym="){
-			pseudonym=value.substring("pseudonym=".length);
-			alert(pseudonym);
-		}	
-	});	
+	readCookie();	
 	
 	var URL = "http://141.19.142.57:5002/profile/";
 	var dataObject = {'token': token, 'getownprofile': pseudonym};
@@ -40,22 +29,9 @@ function loadContacts(){
   	});
 	
 }
-var sequence;
 var date;
 function send(){
-	var decodedCookie = decodeURIComponent(document.cookie);
-    	var ca = decodedCookie.split(';');
-	$.each(ca, function(index, value){
-		value = value.trim();
-		if(value.substring(0,6)=="token="){
-			token=value.substring(6);
-			alert(token);		
-		}
-		if(value.substring(0, "pseudonym=".length)=="pseudonym="){
-			pseudonym=value.substring("pseudonym=".length);
-			alert(pseudonym);
-		}	
-	});	
+	readCookie();
 	var URL = "http://141.19.142.57:5000/send/";
 	var dataObject = {'from': pseudonym, 'to': 'bob', 'date':'2017-03-30T17:00:00Z', 'text': 'Test', 'token': token};
 
@@ -68,13 +44,57 @@ function send(){
 		contentType: "application/json; charset=utf-8",
             	dataType: 'json',
             	success: function(result) {
-		sequence=result.sequence;
+		//TODO: sequenceNumbers von dem to
+		sequenceNumbers[pseudonym]=result.sequence;
 		date=result.date;
 		alert(sequence);
                 alert("success?");},
 		error: function(xhr, a, b){
 		alert(" error");}
   	});
+}
+
+
+var messages;
+function getMessages(){
+	readCookie();
+	//TODO: seuqencenumber vom to anfügen
+	var URL = "http://141.19.142.57:5000/messages/"+pseudonym;
+	var dataObject = {'from': pseudonym, 'to': 'bob', 'date':'2017-03-30T17:00:00Z', 'text': 'Test', 'token': token};
+
+        alert(JSON.stringify(dataObject));
+	$.ajax({
+            	url: URL,
+           	type: 'GET',    
+            	data: JSON.stringify(dataObject),
+		contentType: "application/json; charset=utf-8",
+            	dataType: 'json',
+            	success: function(result) {
+		//TODO:append messages
+		messages=result;
+		alert(messages);
+                alert("success?");},
+		error: function(xhr, a, b){
+		alert(" error");}
+  	});
+
+}
+
+function readCookie(){
+	var decodedCookie = decodeURIComponent(document.cookie);
+    	var ca = decodedCookie.split(';');
+	$.each(ca, function(index, value){
+		value = value.trim();
+		if(value.substring(0,6)=="token="){
+			token=value.substring(6);
+			alert(token);		
+		}
+		if(value.substring(0, "pseudonym=".length)=="pseudonym="){
+			pseudonym=value.substring("pseudonym=".length);
+			alert(pseudonym);
+		}
+	
+	});	
 }
 
     
