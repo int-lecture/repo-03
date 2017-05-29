e2131524bd77f2f710ce6e3d58b9f72129c1a5c3
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-05-29 14:33:51 +0200

    Add files via upload

--- website/chatFrontend/chatApplication/js/loginApplication.js
+++ website/chatFrontend/chatApplication/js/loginApplication.js
@@ -1,25 +1,10 @@
-$( document ).ready(function() {   
-	var URL = "http://141.19.142.57:5001/login";
-	var dataObject = { 'pseudonym': "Hallo", 'password': "1234" };
-	$.ajax({	
-	     	url: URL,
-            	type: 'POST',
-		contentType: "application/json",    
-            	data: JSON.stringify(dataObject),
-            	dataType: 'json',
-            	success: function(result) {
-                alert("success?");},
-	    	error: function(xhr, ajaxOptions, thrownError){
-		alert("error?");
-		alert(xhr.status);}
-        });
-
+$( document ).ready(function() {  
+	
 });
 
 function checkPw(){
-	
-	var URL = "http://141.19.142.57:5001/login/";
-	var dataObject = { 'user': $("#inputEmail").val(), 'password': $("#inputPassword").val()};
+	var URL = "http://141.19.142.57:5002/register/";
+	var dataObject = {'user': $("#inputEmail").val(), 'password': $("#pw1").val(); };
 
         alert(JSON.stringify(dataObject));
 
@@ -27,12 +12,12 @@ function checkPw(){
             	url: URL,
            	type: 'POST',    
             	data: JSON.stringify(dataObject),
+		contentType: "application/json; charset=utf-8",
             	dataType: 'json',
             	success: function(result) {
                 alert("success?")}
-        	}).error(function() {
-    		alert( "Handler for .error() called." )
-  	});;
+  	});
+	alert("Erfolgreich registriert");
 	return true;
 
 }
