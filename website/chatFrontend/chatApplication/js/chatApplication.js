27d3a66d1ba3eaf302ec5c4c50171eacbc4f25be
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-05-29 16:50:48 +0200

    Add files via upload

---
+++ website/chatFrontend/chatApplication/js/chatApplication.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {  
+	loadContacts();
+});
+var token;
+var pseudonym;
+var contacts;
+function loadContacts(){
+	var decodedCookie = decodeURIComponent(document.cookie);
+    	var ca = decodedCookie.split(';');
+	$.each(ca, function(index, value){
+		if(value.substring(0,6)=="token="){
+			token=value.substring(6);
+			alert(token);		
+		}else if(value.substring(0,10)=="pseudonym="){
+			pseudonym=value.substring(10);
+			alert(pseudonym);
+		}	
+	});	
+	
+	var URL = "http://141.19.142.57:5002/profile/";
+	var dataObject = {'token': token, 'getownprofile': pseudonym};
+
+        alert(JSON.stringify(dataObject));
+
+        $.ajax({
+            	url: URL,
+           	type: 'POST',    
+            	data: JSON.stringify(dataObject),
+		contentType: "application/json; charset=utf-8",
+            	dataType: 'json',
+            	success: function(result) {
+		contacts=result.contacts;
+                alert("success?");
+		window.location.href = "http://141.19.142.57/chatFrontend/chatApplication/chatApplication.html";},
+		error: function(xhr, a, b){
+		alert(" error");}
+  	});
+	
+}
+
+    
