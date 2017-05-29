1c0598cdfe91ed814ade7e9483cc65d5aca6c589
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-05-29 11:08:35 +0200

    Add files via upload

--- website/chatFrontend/chatApplication/js/registerApplication.js
+++ website/chatFrontend/chatApplication/js/registerApplication.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {   
-
+	var URL = "http://141.19.142.57:5002/register";
+	var dataObject = { 'pseudonym': "Hallo", 'user': "hallo@web.de", 'password': "1234" };
+$.ajax({	
+	     	url: URL,
+            	type: 'PUT',
+		contentType: "application/json",    
+            	data: JSON.stringify(dataObject),
+            	dataType: 'json',
+            	success: function(result) {
+                alert("success?");},
+	    	error: function(xhr, ajaxOptions, thrownError){
+		alert("error?");
+		alert(xhr.status);}
+        });
 
 });
 
@@ -11,7 +24,26 @@ function checkPw(){
 	if (pw1 != pw2){
 		$("#error").html("Du böser bube die Passwörter sind nicht Identisch!");
 		return false;}
+	
+	var URL = "http://141.19.142.57:5002/register/";
+
+        var dataObject = { 'pseudonym': $("#inputPseudonym").val(), 'user': $("#inputEmail").val(), 'password': pw1 };
+
+        alert(JSON.stringify(dataObject));
+
+        $.ajax({
+            url: URL,
+            type: 'PUT',    
+            data: JSON.stringify(dataObject),
+            dataType: 'json',
+            success: function(result) {
+                alert("success?")}
+        }).error(function() {
+    alert( "Handler for .error() called." )
+  });;
 	return true;
 
 }
+
+
 
