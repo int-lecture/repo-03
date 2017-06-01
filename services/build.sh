6d4570197739ce9614c0dfa28a9accbae231ae5f
MorMundHS-MA <1611229@stud.hs-mannheim.de>
2017-06-01 16:21:39 +0200

    Enforce working test in build script

--- services/build.sh
+++ services/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 git pull
-(cd chat-server/; mvn clean compile assembly:single)
-(cd login-server/; mvn clean compile assembly:single)
-(cd register-server/; mvn clean compile assembly:single)
+(cd chat-server/; mvn clean compile test assembly:single)
+(cd login-server/; mvn clean compile test assembly:single)
+(cd register-server/; mvn clean compile test assembly:single)
 echo "Binaries finished building."
 echo "Deploying."
-mv login-server/target/login-server*.jar login-server.jar
-mv chat-server/target/chat-server*.jar chat-server.jar
-mv register-server/target/register-server*.jar register-server.jar
+cp login-server/target/login-server*.jar .
+cp chat-server/target/chat-server*.jar .
+cp register-server/target/register-server*.jar .
+mv login-server*.jar login-server.jar
+mv chat-server*.jar chat-server.jar
+mv register-server*.jar register-server.jar
 scp *.jar docker-03:/usr/local/source/repo-03/services/
 scp *.service docker-03:/usr/local/source/repo-03/services/
 echo "Deployed."
