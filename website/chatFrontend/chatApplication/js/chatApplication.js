59d5750720c783e8fb62a96ca57a75b6b721857b
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-06-03 14:06:41 +0200

    corrected chat logic and improved user feedback

--- website/chatFrontend/chatApplication/js/chatApplication.js
+++ website/chatFrontend/chatApplication/js/chatApplication.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+    loadConfig();
     //loginTest("bob@web.de", "HalloIchbinBob");
     //loginTest("tom@web.de", "HalloIchbinTom");
     readCookie();
-    openChat("Secure Messenger Demo");
+    openChat("Secure Messenger");
     testContactDiv();
     //loadContacts();
     getMessages();
@@ -10,6 +11,10 @@ $(document).ready(function () {
         $(".side-two").css({
             "left": "0"
         });
+    })
+
+    $(".heading-dot").click(function () {
+        $("#conversation").empty();
     })
 
     $(".newMessage-back").click(function () {
@@ -23,6 +28,10 @@ $(document).ready(function () {
     $(".reply-send").click(function () {
         send();
     })
+    $("#startChatWithFriend").click(function () {
+        openChat($("#newFriendsName"));
+    })
+
 
 });
 var token;
@@ -30,57 +39,100 @@ var pseudonym;
 var contact = [];
 var partner;
 var sequenceNumbers = [];
-//var ipLogin = ip;
-//var ipRegister = ip;
-//var ipChat = ip;
+var sequenceNumber;
+var ipLogin;
+var ipRegister;
+var ipChat;
 var messages = [];
+var sentMessages = [];
 var tokenBob;
 var tokenTom;
 var date;
+var chatMessages = [];
 
+function openThisChat(id) {
+    openChat(id);
+}
+function openNewChat() {
+    var newFriendsName = $("#newFriendsName").val();
+    openChat(newFriendsName);
+    //newContact(newFriendsName);
+    $(".side-two").css({
+        "left": "-100%"
+    });
+}
+
+function loadConfig() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var ips = xhr.responseText.split(";");
+            ipLogin = ips[0].substring("ipLogin:".length + 1);
+            ipChat = ips[1].substring("ipChat:".length + 1);
+            ipRegister = ips[2].substring("ipRegister:".length + 1);
+        }
+    }
+    xhr.open('GET', 'http://141.19.142.57/chatFrontend/chatApplication/js/config.txt', false);
+    xhr.send();
+}
 
 function openChat(partner) {
-    if (partner == "Secure Messenger Demo") {
+    if (partner == "Secure Messenger") {
         $("#conversation").append("<div class='row message-body'><div class='col-sm-12 message-main-receiver'><div class='receiver'><div class='message-text' id='messages'>Hey willkommen beim Secure Messenger neben siehst du deine bisherigen Freunde</div><span class='message-time pull-right'>~42~</span></div></div></div></div>");
-
     } else {
         this.partner = partner;
-        //$(".conversation").empty();
-        //$(".conversation").append("<div class='row heading'><div class='col-sm-2 col-md-1 col-xs-3 heading-avatar'><div class='heading-avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-8 col-xs-7 heading-name'><a class='heading-name-meta'>"+partner+"</a><span class='heading-online'>Online</span></div><div class='col-sm-1 col-xs-1  heading-dot pull-right'><i class='fa fa-ellipsis-v fa-2x  pull-right' aria-hidden='true'></i></div></div>");
         document.getElementById("partner").innerHTML = partner;
-        getMessages();
+        if (typeof messages == 'undefined') {
+            getMessages();
+        }
         showMessages();
     }
 }
-function send() {
-    readCookie();
-    ip = "141.19.142.57";
-    var URL = "http://" + ip + ":5000/send/";
-    var dataObject = { 'from': pseudonym, 'to': partner, 'date': '2017-03-30T17:00:00+0200', 'token': token, 'text': $("#comment").val() };
 
-    $.ajax({
-        url: URL,
-        type: 'PUT',
-        data: JSON.stringify(dataObject),
-        contentType: "application/json; charset=utf-8",
-        dataType: 'json',
-        success: function (result) {
-            sequenceNumbers[pseudonym] = result.sequence;
-            var sentMessage = {
-                from: pseudonym, to: partner
-                , date: '2017-03-30T17:00:00+0200', text: $("#comment").val(), sequence: 0
-            };
-            messages = messages.concat(sentMessage);
-            $("#comment").val("");
-            showMessages();
-            //date = result.date;
-        },
-        error: function (xhr, a, b) {
-            alert(text + " wurde nicht erfolgreich an " + partner + " gesendet");
-        }, async: false
-
-    });
+function getLegalDate() {
+    var date = new Date();
+    var stringDate = date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()) + "-" + (date.getDay() < 10 ? "0" + date.getDay() : date.getDay()) + "T" + (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" + ((date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())) + "+0200";
+    return stringDate;
 }
+
+function send() {
+    if (document.getElementById("partner").innerHTML == "Secure Messenger") {
+        $("#conversation").append("<div class='row message-body'><div class='col-sm-12 message-main-receiver'><div class='receiver'><div class='message-text' id='messages'>Du kannst nicht mit uns schreiben, wähle bitte einen deiner Kontakte aus deiner Kontaktliste aus oder suche über die Sprechblase nach neuen Freunden</div><span class='message-time pull-right'>~42~</span></div></div></div></div>");
+        getMessages();
+    } else if ($("#comment").val() == "") {
+
+    } else {
+        readCookie();
+        var URL = ipChat + "/send/";
+        var dataObject = { 'from': pseudonym, 'to': partner, 'date': getLegalDate(), 'token': token, 'text': $("#comment").val() };
+        $.ajax({
+            url: URL,
+            type: 'PUT',
+            data: JSON.stringify(dataObject),
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            success: function (result) {
+                sequenceNumber = result.sequence;
+                var sentMessage = {
+                    from: pseudonym, to: partner
+                    , date: getLegalDate(), text: $("#comment").val(), sequence: result.sequence
+                };
+                sentMessages.push(sentMessage);
+                $("#comment").val("");
+                showMessages();
+            },
+            error: function (xhr, a, b) {
+                alert("Die Nachricht wurde nicht erfolgreich an " + partner + " gesendet");
+            }
+
+        });
+    }
+}
+function newContact(newChatPartner) {
+    $(".sideBar").append("<div class='row sideBar-body' ><div class='col-sm-3 col-xs-3 sideBar-avatar'><div class='avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-9 col-xs-9 sideBar-main' id='" + newChatPartner + "'><div class='row'><div class='col-sm-8 col-xs-8 sideBar-name'><span class='name-meta' id='contacts'>" + newChatPartner + "</span></div><div class='col-sm-4 col-xs-4 pull-right sideBar-time'><span class='time-meta pull-right'>18:18</span></div></div></div></div>");
+
+}
+
 function testContactDiv() {
     $(".sideBar").append("<div class='row sideBar-body'><div class='col-sm-3 col-xs-3 sideBar-avatar'><div class='avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-9 col-xs-9 sideBar-main' id='tom'><div class='row'><div class='col-sm-8 col-xs-8 sideBar-name'><span class='name-meta' id='contacts'>tom</span></div><div class='col-sm-4 col-xs-4 pull-right sideBar-time'><span class='time-meta pull-right'>18:18</span></div></div></div></div>");
     $(".sideBar").append("<div class='row sideBar-body'><div class='col-sm-3 col-xs-3 sideBar-avatar'><div class='avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-9 col-xs-9 sideBar-main' id='bob'><div class='row'><div class='col-sm-8 col-xs-8 sideBar-name'><span class='name-meta' id='contacts'>bob</span></div><div class='col-sm-4 col-xs-4 pull-right sideBar-time'><span class='time-meta pull-right'>18:18</span></div></div></div></div>");
@@ -90,7 +142,7 @@ function testContactDiv() {
 
 function loadContacts() {
     ip = "141.19.142.57";
-    var URL = "http://" + ip + ":5002/profile/";
+    var URL = ipRegister + "/profile/";
     var dataObject = { 'getownprofile': pseudonym, 'token': token };
 
     $.ajax({
@@ -100,12 +152,11 @@ function loadContacts() {
         contentType: "application/json; charset=utf-8",
         dataType: 'json',
         success: function (result) {
-            //alert(result.contact);
             contact = result.contact;
         },
         error: function (xhr, a, b) {
             alert("Kontakte wurden nicht erfolgreich geladen");
-        }, async: false
+        }
 
     });
     $.each(contact, function (index, value) {
@@ -115,43 +166,57 @@ function loadContacts() {
 }
 
 function getMessages() {
-    ip = "141.19.142.57";
-    //Not Tested TODO: seuqencenumber vom to anfügen
-    if (typeof (sequenceNumbers[pseudonym]) == 'undefined') {
-        var URL = "http://" + ip + ":5000/messages/" + pseudonym + "/";
-    } else {
-        var URL = "http://" + ip + ":5000/messages/" + pseudonym + "/" + sequenceNumbers[pseudonym].toString();
-    }
-    $.ajax({
-        headers: {
-            "Authorization": token
-        },
-        url: URL,
-        type: 'GET',
-        contentType: "application/json; charset=utf-8",
-        dataType: 'json',
-        success: function (result, textStatus, xhr) {
-            //TODO:append messages
-            if (xhr.status == 200) {
-                //alert("getMessages von " + pseudonym + " erfolgreich erhalten");
-                messages = result;
-                $.each(result, function (index, value) {
-                    messages = messages.concat(value);
-                });
-                showMessages();
-            }
-        },
-        error: function (xhr, a, b) {
-            //alert(" error");
-            alert("getMessages von " + pseudonym + " fehlgeschlagen");
-        }, async: false
-    });
+    function update() {
+        if (typeof sequenceNumber != 'undefined') {
+            var URL = ipChat + "/messages/" + pseudonym + "/0"
+            //+ sequenceNumber.toString();
+        } else {
+            var URL = ipChat + "/messages/" + pseudonym + "/0";
+        }
+        $.ajax({
+            headers: {
+                "Authorization": token
+            },
+            url: URL,
+            type: 'GET',
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            success: function (result, textStatus, xhr) {
+                if (xhr.status == 200) {
+                    if (typeof sequenceNumber == 'undefined') {
+                        messages = result;
+                        showMessages();
+                    }
+                    else {
+                        messages = result;
+                        sequenceNumber = sequenceNumber + result.length;
+                        showMessages();
+                    }
+                } else if (xhr.status == 204) {
 
+                }
+            },
+            error: function (xhr, a, b) {
+                window.location.href = "loginApplication.html";
+                //alert("getMessages von " + pseudonym + " fehlgeschlagen");
+            }
+
+        });
+
+    }
+    setInterval(update, 10000);
 }
 
+function sortMessages() {
+    chatMessages = sentMessages.concat(messages);
+    chatMessages.sort(function (a, b) {
+        return (a.date < b.date) ? -1 : ((a.date > b.date) ? 1 : 0);
+    });
+}
 function showMessages() {
+    sortMessages();
     $("#conversation").empty();
-    $.each(messages, function (index, value) {
+    $.each(chatMessages, function (index, value) {
         if (value.from == partner && value.to == pseudonym) {
             $("#conversation").append("<div class='row message-body'><div class='col-sm-12 message-main-receiver'><div class='receiver'><div class='message-text' id='messages'>" + value.text + "</div><span class='message-time pull-right'>" + value.date.substr(11, 5) + "</span></div></div></div></div>");
         }
@@ -175,39 +240,16 @@ function readCookie() {
         value = value.trim();
         if (value.substring(0, "token=".length) == "token=") {
             token = value.substring(6);
-            //alert("token aus dem Cookie: "+token);		
         }
         if (value.substring(0, "pseudonym=".length) == "pseudonym=") {
             pseudonym = value.substring("pseudonym=".length);
-            //alert(pseudonym);
         }
-        if (value.substring(0, "ip=".length) == "ip=") {
-            ip = value.substring("ip=".length);
-            //alert(ip);
-        }
-        if (value.substring(0, "ipLogin=".length) == "ipLogin=") {
-            ipLogin = value.substring("ipLogin=".length);
-        }
-        if (value.substring(0, "ipRegister=".length) == "ipRegister=") {
-            ipRegister = value.substring("ipRegister=".length);
-            //alert(ip);
-        }
-        if (value.substring(0, "ipChat=".length) == "ipChat=") {
-            ipChat = value.substring("ipChat=".length);
-            //alert(ip);
-        }
-
-
     });
 }
 
 function loginTest(user, password) {
-    ip = "141.19.142.57";
-    var URL = "http://" + ip + ":5001/login/";
+    var URL = ipLogin + "/login/";
     var dataObject = { 'user': user, 'password': password };
-
-    //alert(JSON.stringify(dataObject));
-
     $.ajax({
         url: URL,
         type: 'POST',
@@ -215,13 +257,13 @@ function loginTest(user, password) {
         contentType: "application/json; charset=utf-8",
         dataType: 'json',
         success: function (result) {
-            //alert(result.token);
             document.cookie = "token=" + result.token;
             document.cookie = "pseudonym=" + result.pseudonym;
-            //document.cookie="expires="+result["expire-date"];
         },
         error: function (xhr, a, b) {
         }, async: false
     });
 }
+
+
 
