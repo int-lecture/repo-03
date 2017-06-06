f4ca44f45c942553b60bc21a18b930bc08257581
MorMundHS-MA <1611229@stud.hs-mannheim.de>
2017-06-06 19:23:31 +0200

    Fixed build script #3

--- services/build.sh
+++ services/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git pull
+rm login-server/target/login-server*.jar
+rm chat-server/target/chat-server*.jar
+rm register-server/target/register-server*.jar
 (cd common/; mvn clean compile install)
 (cd login-server/; mvn clean compile test install assembly:single)
 (cd chat-server/; mvn clean compile test install assembly:single)
@@ -7,9 +10,6 @@ git pull
 
 echo "Binaries finished building."
 echo "Deploying."
-rm login-server/target/login-server*.jar
-rm chat-server/target/chat-server*.jar
-rm register-server/target/register-server*.jar
 mv login-server/target/login-server*.jar login-server.jar
 mv chat-server/target/chat-server*.jar chat-server.jar
 mv register-server/target/register-server*.jar register-server.jar
