6183372325fff8ba246351fef83a74e6f9b59a49
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-06-14 18:13:26 +0200

    fixed sequencenumber

--- website/secure_messenger/js/chatApplication.js
+++ website/secure_messenger/js/chatApplication.js
@@ -117,7 +117,6 @@ function send() {
             contentType: "application/json; charset=utf-8",
             dataType: 'json',
             success: function (result) {
-                sequenceNumber = result.sequence;
                 var sentMessage = {
                     from: pseudonym, to: partner
                     , date: getLegalDate(), text: $("#comment").val(), sequence: result.sequence
@@ -164,14 +163,15 @@ function loadContacts() {
 
     });
     $.each(contact, function (index, value) {
-        $(".sideBar").append("<div class='row sideBar-body'><div class='col-sm-3 col-xs-3 sideBar-avatar'><div class='avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-9 col-xs-9 sideBar-main'><div class='row'><div class='col-sm-8 col-xs-8 sideBar-name'><span class='name-meta' id='contacts'>" + value + "</span></div><div class='col-sm-4 col-xs-4 pull-right sideBar-time'><span class='time-meta pull-right'>18:18</span></div></div></div></div>");
-
+    $(".sideBar").append("<div class='row sideBar-body' ><div class='col-sm-3 col-xs-3 sideBar-avatar'><div class='avatar-icon'><img src='css/profilePic.png'></div></div><div class='col-sm-9 col-xs-9 sideBar-main' id='" + value + "'><div class='row'><div class='col-sm-8 col-xs-8 sideBar-name'><span class='name-meta' id='contacts'>" + value + "</span></div><div class='col-sm-4 col-xs-4 pull-right sideBar-time'><span class='time-meta pull-right'>18:18</span></div></div></div></div>");
+    $("#"+value).click(function(){
+        openChat($(this).attr('id'));
+    });
     });
 }
 
 function getMessages() {
     function update() {
-            alert(sequenceNumber);
             var URL = ipChat + "/messages/" + pseudonym + "/"+sequenceNumber;
         $.ajax({
             headers: {
