8f12b342f74e68f6089bcb056536ed131c193911
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-06-17 14:22:36 +0200

    small changes

--- website/secure_messenger/js/loginApplication.js
+++ website/secure_messenger/js/loginApplication.js
@@ -1,46 +1,46 @@
-$( document ).ready(function() {  
+$(document).ready(function () {
 	loadConfig();
 	startConnection();
 });
-$(document).keypress(function(e){
-    if(e.which==13){
-        checkPw();
-    }
+$(document).keypress(function (e) {
+	if (e.which == 13) {
+		checkPw();
+	}
 })
 
-function startConnection(){
-	var URL = ipLogin+"/login/";
+function startConnection() {
+	var URL = ipLogin + "/login/";
 	$.ajax({
-			url: URL,
-			type: 'OPTIONS',    
-			success: function(result) {
-			},
-			error: function(xhr, ajaxOptions, thrownError){
-			}
-  	});
+		url: URL,
+		type: 'OPTIONS',
+		success: function (result) {
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+		}
+	});
 }
-function checkPw(){
-	var URL = ipLogin+"/login/";
-	var dataObject = {'user': $("#inputEmail").val(), 'password': $("#inputPassword").val()};
+function checkPw() {
+	var URL = ipLogin + "/login/";
+	var dataObject = { 'user': $("#inputEmail").val(), 'password': $("#inputPassword").val() };
 
-        //alert(JSON.stringify(dataObject));
+	//alert(JSON.stringify(dataObject));
 
-        $.ajax({
-			url: URL,
-			type: 'POST',    
-			data: JSON.stringify(dataObject),
-			contentType: "application/json; charset=utf-8",
-			dataType: 'json',
-			success: function(result) {
-				document.cookie = "token="+result.token;
-				document.cookie="pseudonym="+result.pseudonym+";expires="+result["expire-date"];
-				//alert("success?");
-				window.location.href = "chatApplication.html";
-			},
-			error: function(xhr, ajaxOptions, thrownError){
-				//alert(" error");
-			}
-  	});
+	$.ajax({
+		url: URL,
+		type: 'POST',
+		data: JSON.stringify(dataObject),
+		contentType: "application/json; charset=utf-8",
+		dataType: 'json',
+		success: function (result) {
+			document.cookie = "token=" + result.token;
+			document.cookie = "pseudonym=" + result.pseudonym + ";expires=" + result["expire-date"];
+			//alert("success?");
+			window.location.href = "chatApplication.html";
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+			//alert(" error");
+		}
+	});
 	return false;
 
 }
