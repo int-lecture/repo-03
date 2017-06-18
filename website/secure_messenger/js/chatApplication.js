7367a6e027ad87f55a97e3bc509ad080a1773d1a
lucaSchilling <1610927@stud.hs-mannheim.de>
2017-06-18 11:48:45 +0200

    fixed really strange error in getLegalDate()

--- website/secure_messenger/js/chatApplication.js
+++ website/secure_messenger/js/chatApplication.js
@@ -103,7 +103,7 @@ function openChat(partner) {
 
 function getLegalDate() {
     var date = new Date();
-    var stringDate = date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth()) + "-" + (date.getDay() < 10 ? "0" + date.getDay() : date.getDay()) + "T" + (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" + ((date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())) + "+0200";
+    var stringDate = date.getFullYear() + "-" + ((date.getMonth()+1) < 10 ? "0" + (date.getMonth()+1) : (date.getMonth()+1)) + "-" + ((date.getDate()) < 10 ? "0" + (date.getDate()) : (date.getDate())) + "T" + (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" + ((date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())) + "+0200";
     return stringDate;
 }
 
