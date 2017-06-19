98881613a6ce4b5511de64e03abfc42931c5a298
MorMundHS-MA <1611229@stud.hs-mannheim.de>
2017-06-19 18:55:09 +0200

    Updated docker build script

--- services/build.sh
+++ services/build.sh
@@ -4,3 +4,10 @@ git pull
 (cd login-server/; mvn clean compile test install assembly:single && sudo docker build -t loginserver .)
 (cd chat-server/; mvn clean compile test install assembly:single && sudo docker build -t chatserver .)
 (cd register-server/; mvn clean compile install test assembly:single && sudo docker build -t registerserver .)
+sudo docker tag loginserver d3adlysurprise/loginserver
+sudo docker tag loginserver d3adlysurprise/registerserver
+sudo docker tag loginserver d3adlysurprise/chatserver
+echo "Uploading docker images."
+sudo docker push d3adlysurprise/loginserver
+sudo docker push d3adlysurprise/registerserver
+sudo docker push d3adlysurprise/chatserver
